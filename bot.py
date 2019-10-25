123179d073fa97f7d9c2fb2f65b5faeb2a38484b
Tushar Verma <tushar.m.verma@gmail.com>
2019-10-24 20:25:55 -0400

    Add logic

--- bot.py
+++ bot.py
@@ -24,3 +24,53 @@ class Bot(object):
 
         self._msg_count = 0
         self.n_msg_per_sec = n_msg_per_sec
+
+    def connect(self, channel):
+        self._socket = socket.socket()
+        self._socket.connect((HOST, PORT))
+        self._socket.send("PASS {}\r\n".format(PASS).encode("utf-8"))
+        self._socket.send("NICK {}\r\n".format(NICK).encode("utf-8"))
+        self._socket.send("JOIN {}\r\n".format(channel).encode("utf-8"))
+
+    def chat(self, msg):
+        self._socket.send("PRIVMSG {} :{}\r\n".format(self.channel, msg))
+
+    def _ping_pong(self, response):
+        if response == "PING :tmi.twitch.tv\r\n":
+            # send pong back to prevent timeout
+            self._socket.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+            return True
+        else:
+            return False
+
+    def _get_response(self):
+        try:
+            response = self._socket.recv(1024).decode("utf-8")
+        except UnicodeDecodeError as e:
+            print('\n\n%s\n\n' % e)
+            return False
+
+        if self._ping_pong(response):
+            return False
+        elif ':tmi.twitch.tv' in response:
+            return False
+        else:
+            return response
+
+    def _process_msg(self, response):
+        username = re.search(r"\w+", response).group(0)
+        mask = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+        message = mask.sub("", response).strip('\r\n')
+        return username, message
+
+    def action(self, username, msg):
+        return NotImplementedError()
+
+    def run(self):
+        while True:
+            response = self._get_response()
+            if response:
+                username, msg = self._process_msg(response)
+                self.action(username, msg)
+
+            sleep(1 / float(self.n_msg_per_sec))
