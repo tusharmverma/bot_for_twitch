c006bf8b4987651110cf67ce5c325fae39266a4f
Tushar Verma <tushar.m.verma@gmail.com>
2020-06-11 20:27:04 -0400

    count bot class

---
+++ markov.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import nltk
+import yaml
+import re
+
+import pymongo
+from pymongo import MongoClient
+
+
+class Transition(object):
+    """"""
+
+
+class MarkovModel(object):
+    
+
+
+
+if __name__ == '__main__':
+    mm = test_anime_synopsis(n_synopsis=10000, order=4)
+    print(mm.generate_text(50))
